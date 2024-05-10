c00c029b98d6961a05aca8f6b9c856121289067e
ma1kery <lungu2003mihai1@mail.ru>
2024-05-10 20:59:50 +0300

    Added Cookie

--- MusicApp.BusinessLogic/Core/UserApi.cs
+++ MusicApp.BusinessLogic/Core/UserApi.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using AutoMapper;
 using MusicApp.BusinessLogic.DBModel;
 using MusicApp.Domain.Entities.User;
+using MusicApp.Domain.Enums;
+using MusicApp.Helpers;
 
 namespace MusicApp.BusinessLogic.Core
 {
@@ -14,13 +20,88 @@ namespace MusicApp.BusinessLogic.Core
         {
             using (var userContext = new UserContext())
             {
-                var result = userContext.Users.FirstOrDefault(u => u.Username == data.Credential && u.Password == data.Password);
+                var result = userContext.Users.FirstOrDefault(u => u.Username == data.Credential && u.Password == data.Password && u.Level == URole.Moderator);
                 if (result == null)
                 {
                     return new ULoginResp { Status = false };
                 }
             }
             return new ULoginResp { Status = true };
+        }
+        internal HttpCookie Cookie(string loginCredential)
+        {
+            var apiCookie = new HttpCookie("X-KEY")
+            {
+                Value = CookieGenerator.Create(loginCredential)
+            };
+
+            using (var db = new SessionContext())
+            {
+                Session curent;
+                var validate = new EmailAddressAttribute();
+                if (validate.IsValid(loginCredential))
+                {
+                    curent = (from e in db.Sessions where e.Username == loginCredential select e).FirstOrDefault();
+                }
+                else
+                {
+                    curent = (from e in db.Sessions where e.Username == loginCredential select e).FirstOrDefault();
+                }
+
+                if (curent != null)
+                {
+                    curent.CookieString = apiCookie.Value;
+                    curent.ExpireTime = DateTime.Now.AddMinutes(60);
+                    using (var todo = new SessionContext())
+                    {
+                        todo.Entry(curent).State = EntityState.Modified;
+                        todo.SaveChanges();
+                    }
+                }
+                else
+                {
+                    db.Sessions.Add(new Session
+                    {
+                        Username = loginCredential,
+                        CookieString = apiCookie.Value,
+                        ExpireTime = DateTime.Now.AddMinutes(60)
+                    });
+                    db.SaveChanges();
+                }
+            }
+
+            return apiCookie;
+        }
+
+        internal UserMinimal UserCookie(string cookie)
+        {
+            Session session;
+            UDBTable curentUser;
+
+            using (var db = new SessionContext())
+            {
+                session = db.Sessions.FirstOrDefault(s => s.CookieString == cookie && s.ExpireTime > DateTime.Now);
+            }
+
+            if (session == null) return null;
+            using (var db = new UserContext())
+            {
+                var validate = new EmailAddressAttribute();
+                if (validate.IsValid(session.Username))
+                {
+                    curentUser = db.Users.FirstOrDefault(u => u.Email == session.Username);
+                }
+                else
+                {
+                    curentUser = db.Users.FirstOrDefault(u => u.Username == session.Username);
+                }
+            }
+
+            if (curentUser == null) return null;
+            Mapper.Initialize(cfg => cfg.CreateMap<UDBTable, UserMinimal>());
+            var userminimal = Mapper.Map<UserMinimal>(curentUser);
+
+            return userminimal;
         }
     }
 }
