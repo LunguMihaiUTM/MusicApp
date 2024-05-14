bf5a3e7f3bd8e18b849e97aa759203e08a5775c6
ma1kery <lungu2003mihai1@mail.ru>
2024-05-14 13:13:39 +0300

    Add Context for Session

---
+++ MusicApp.BusinessLogic/DBModel/SessionContext.cs
@@ -0,0 +1,19 @@
+﻿using MusicApp.Domain.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicApp.BusinessLogic.DBModel
+{
+    public class SessionContext : DbContext
+    {
+        public SessionContext() : base("name=MusicApp")
+        {
+        }
+
+        public virtual DbSet<Session> Sessions { get; set; }
+    }
+}
