2756e2e693533df3bf59346efaf5aaf7fa23f528
ma1kery <lungu2003mihai1@mail.ru>
2024-05-17 11:25:45 +0300

    Add class for song

---
+++ MusicApp.Domain/Entities/Music/Song.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicApp.Domain.Entities.Music
+{
+    public class Song
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public byte[] Mp3File { get; set; }
+        public string Mp3FileName { get; set; }
+        public byte[] Image { get; set; }
+        public string ImageFileName { get; set; }
+    }
+}
---
+++ MusicApp.Domain/Entities/Music/SongTables.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicApp.Domain.Entities.Music
+{
+    public class SongTables
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Artist { get; set; }
+
+        [Required]
+        public byte[] Mp3File { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Mp3FileName { get; set; }
+
+        [Required]
+        public byte[] Image { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string ImageFileName { get; set; }
+    }
+}
