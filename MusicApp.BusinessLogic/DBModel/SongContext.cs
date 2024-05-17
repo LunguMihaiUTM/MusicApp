c579d0a0c2f728e96ad7a6947ccd16394f98c2ac
ma1kery <lungu2003mihai1@mail.ru>
2024-05-17 11:24:04 +0300

    Add Context for Songs

---
+++ MusicApp.BusinessLogic/DBModel/SongContext.cs
@@ -0,0 +1,22 @@
+﻿using MusicApp.Domain.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicApp.Domain.Entities.Music;
+
+namespace MusicApp.BusinessLogic.DBModel
+{
+    public class SongContext : DbContext
+    {
+        public SongContext() :
+            base("name = MusicApp") // connectionstring name define in your web.config
+        {
+
+        }
+
+        public virtual DbSet<SongTables> Songs { get; set; }
+    }
+}
