e2d5499dcf3e362441438ccdd73d98627c7de668
ma1kery <lungu2003mihai1@mail.ru>
2024-05-17 11:24:39 +0300

    Add Registration

--- MusicApp.BusinessLogic/Core/UserApi.cs
+++ MusicApp.BusinessLogic/Core/UserApi.cs
@@ -18,16 +18,92 @@ namespace MusicApp.BusinessLogic.Core
     {
         internal ULoginResp UserLoginAction(ULoginData data)
         {
-            using (var userContext = new UserContext())
+            UDBTable result;
+            var validate = new EmailAddressAttribute();
+            if (validate.IsValid(data.Credential))
             {
-                var result = userContext.Users.FirstOrDefault(u => u.Username == data.Credential && u.Password == data.Password && u.Level == URole.Moderator);
+                var pass = LoginHelper.HashGen(data.Password);
+                using (var db = new UserContext())
+                {
+                    result = db.Users.FirstOrDefault(u => u.Email == data.Credential && u.Password == pass);
+                }
+
                 if (result == null)
                 {
-                    return new ULoginResp { Status = false };
+                    return new ULoginResp { Status = false, StatusMsg = "The Username or Password is Incorrect" };
                 }
+
+                using (var todo = new UserContext())
+                {
+                    result.LastIp = data.LoginIp;
+                    result.LastLogin = data.LoginDateTime;
+                    todo.Entry(result).State = EntityState.Modified;
+                    todo.SaveChanges();
+                }
+
+                return new ULoginResp { Status = false };
             }
-            return new ULoginResp { Status = true };
+            else
+            {
+                var pass = LoginHelper.HashGen(data.Password);
+                using (var db = new UserContext())
+                {
+                    result = db.Users.FirstOrDefault(u => u.Username == data.Credential && u.Password == pass);
+                }
+
+                if (result == null)
+                {
+                    return new ULoginResp { Status = false, StatusMsg = "The Username or Password is Incorrect" };
+                }
+
+                using (var todo = new UserContext())
+                {
+                    result.LastIp = data.LoginIp;
+                    result.LastLogin = data.LoginDateTime;
+                    todo.Entry(result).State = EntityState.Modified;
+                    todo.SaveChanges();
+                }
+
+                return new ULoginResp { Status = true };
+            }
         }
+
+        internal URegResp UserRegisterAction(URegData data)
+        {
+            using (var userContext = new UserContext())
+            {
+                // Check for duplicate usernames
+                if (userContext.Users.Any(u => u.Username == data.Credential))
+                {
+                    return new URegResp { Status = false, StatusMsg = "Username already exists" };
+                }
+
+                // Check for duplicate emails
+                if (userContext.Users.Any(u => u.Email == data.Email))
+                {
+                    return new URegResp { Status = false, StatusMsg = "Email already registered" };
+                }
+
+                var user = new UDBTable
+                {
+                    Username = data.Credential,
+                    Password = LoginHelper.HashGen(data.Password), // Hash the password before storing
+                    Email = data.Email,
+                    LastLogin = data.RegDateTime,
+                    LastIp = data.RegIp,
+                };
+
+                userContext.Users.Add(user);
+                userContext.SaveChanges();
+
+                return new URegResp { Status = true }; // Registration successful
+            }
+        }
+
+
+
+
+
         internal HttpCookie Cookie(string loginCredential)
         {
             var apiCookie = new HttpCookie("X-KEY")
