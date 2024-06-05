44c0570c94e5f656efbe76f511c03b7f17e6b62e
Alexei Mereniuc <alexei.mereniuc@isa.utm.md>
2024-06-05 15:57:17 +0300

    Add new controllers

---
+++ MusicApp.Web/Controllers/FeaturedPlaylistController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicApp.Web.Controllers
+{
+    public class FeaturedPlaylistController : Controller
+    {
+        // GET: FeaturedPlaylist
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
